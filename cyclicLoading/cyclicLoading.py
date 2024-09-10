2f755ab33b407439c63fe4b25f5a688ee34ad208
Nicolas Mora Bowen <nmorabowen@gmail.com>
2024-09-10 12:57:05 -0300

    fix time inconsistency

--- cyclicLoading/cyclicLoading.py
+++ cyclicLoading/cyclicLoading.py
@@ -65,7 +65,7 @@ class cyclicLoading:
         cycles = np.arange(0, total_cycles + 1, 0.50)
 
         # Create the time array, scaled such that the last time value is 1
-        time = np.linspace(0, 1, len(cycles))
+        time=cycles/total_cycles
         
         # Verbose output if required
         if self.verbose:
